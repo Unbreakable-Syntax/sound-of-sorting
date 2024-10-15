2b9892ff05f67100cd51a5149f4b061c3aa4fb61
Unbreakable-Syntax <louwenngatusani@yahoo.com>
2024-10-15 22:27:24 +0800

    Improved Auxiliary Grail Sort
    O(logn) buffer apparently isn't going to take much effect, 10% array size buffer should provide some noticeable improvements without adding too much overhead.

--- src/algorithms/grailsort.cpp
+++ src/algorithms/grailsort.cpp
@@ -52,7 +52,6 @@
 #include <iterator>
 #include <type_traits>
 #include <utility>
-#include <cmath>
 
 namespace grailsort_detail
 {
@@ -1333,7 +1332,7 @@ void GrailSort(SortArray& A)
 
 void AuxGrailSort(SortArray& A)
 {
-    size_t buf_size = log(A.size());
+    size_t buf_size = A.size() / 10;
     if (buf_size == 0) { buf_size = 1; }
     std::vector<ArrayItem> space_buf(buf_size, ArrayItem(0));
     grailsort(MyIterator(&A, 0), MyIterator(&A, A.size()), space_buf.begin(), space_buf.end());
