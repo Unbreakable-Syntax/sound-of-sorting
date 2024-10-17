e133f4a3c4f5ed91b037862185a3578ea431ed7a
Unbreakable-Syntax <louwenngatusani@yahoo.com>
2024-10-17 20:54:00 +0800

    Fixed minor bug on step visualization
    The very first item of the array during step visualization is ignored, which is not intended, this small change fixes that issue.

--- src/WSortView.cpp
+++ src/WSortView.cpp
@@ -252,7 +252,7 @@ void WSortView::paint(wxDC& dc, const wxSize& dcsize)
         for (size_t i = 0; i < size; ++i)
         {
             int clr = m_array.GetIndexColor(i);
-            if (i == i_step)
+            if (i == i_step || i == 0)
             {
                 int numBrushes = sizeof(brushes) / sizeof(brushes[0]);
                 clr = clr % numBrushes;
