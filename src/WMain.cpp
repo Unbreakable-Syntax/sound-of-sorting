0858fa19495d249823fe89fca4b71727bafaf19b
Unbreakable-Syntax <louwenngatusani@yahoo.com>
2024-10-21 16:40:21 +0800

    Set SortAlgoThread to max priority, increased smallest allowed delay
    I have spent several hours trying to resolve the "cannot terminate thread" error, and I have not been able to come up with any good working solution. I have used several commands and other things to try and prevent the issue from happening, and while I did learn a lot from all of my attempts, I am actually sad that I still failed to fix the problem.
    
    The "cannot terminate thread" happens more prevalently on very small delays, so I decided to prevent the speed slider from going down to 0 ms. I also made the thread go to maximum priority in hopes of allowing the thread to execute (and end) faster, I have been able to deduce that using Delete() will make the TestDestroy() flag become true, and it might help in solving the problem, but I don't know what else I could do in the meantime.

--- src/WMain.cpp
+++ src/WMain.cpp
@@ -153,6 +153,7 @@ bool WMain::RunAlgorithm()
 
         m_thread_terminate = false;
         m_thread->Create();
+        m_thread->SetPriority(WXTHREAD_MAX_PRIORITY);
 
         g_algo_running = true;
         m_thread->Run();
@@ -326,7 +327,7 @@ void WMain::SetDelay(size_t pos)
     g_delay = pow(base, pos / 2000.0 * log(2 * 1000.0) / log(base));
     
 #endif
-    if (pos == 0) g_delay = 0;
+    if (pos == 0) g_delay = 0.1;
 
     if (g_delay > 10)
         labelDelayValue->SetLabel(wxString::Format(_("%.0f ms"), g_delay));
